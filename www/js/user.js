878acea61e6ce742330a141fb89b243210da02ca
Gerrit Sturm <g.sturm@sturm-busse.de>
2012-12-13 15:54:44 +0100

    another test

--- www/js/user.js
+++ www/js/user.js
@@ -261,8 +261,8 @@ $("#serverTest").live("click", function() {										// sending user input to FH
 	}
 	if($('#serverPassword').val() != '') {
 		xhrTest.withCredentials = true;
-		xhrTest.setRequestHeader( 'Authorization', 'Basic dGlnaTphc3VyYTp4');
-//		xhrTest.setRequestHeader( 'Authorization', 'Basic '+$.base64.encode($('#serverUsername').val()+':'+$('#serverPassword').val()+':x') );
+		xhrTest.setRequestHeader( 'Origin', 'http://192.168.178.2');
+		xhrTest.setRequestHeader( 'Authorization', 'Basic '+$.base64.encode($('#serverUsername').val()+':'+$('#serverPassword').val()+':x') );
 	}
 	xhrTest.send(null);
 });
